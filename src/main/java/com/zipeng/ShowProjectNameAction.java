1dd4d0ca0221795f7735c1d478387f249ba19108
liangzipeng <961163289@qq.com>
2023-11-13 15:30:28 +0800

    IDEA 弹窗插件：显示项目名称

---
+++ src/main/java/com/zipeng/ShowProjectNameAction.java
@@ -0,0 +1,16 @@
+package com.zipeng;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+/**
+* Created by IntelliJ IDEA.
+* @Author : liang.zi.peng
+* @create 2023-11-13 10:43
+*/public class ShowProjectNameAction extends AnAction {
+
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+        // TODO: insert action logic here
+    }
+}
